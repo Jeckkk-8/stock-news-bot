c15e0135567a7b6621f8ccd4989003e9acb4b43e
Jeckkk-8 <dechason@hotmail.com>
2026-03-11 10:38:34 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN="8692112101:AAHa6X-3bCXpzRRsrbNGFNAM9-W6nLGNaHo"
+CHAT_ID="8386698996"
+
+STOCKS=[
+"NVDA","AMZN","LLY","CRWD","PLTR","TSM",
+"GOOGL","RKLB","MSFT","AVGO","META",
+"RBRK","VRT","CEG","OKLO"
+]
+
+RSS_FEEDS=[
+"https://feeds.reuters.com/reuters/businessNews",
+"https://www.cnbc.com/id/100003114/device/rss/rss.html",
+"https://finance.yahoo.com/news/rssindex",
+"https://seekingalpha.com/market_currents.xml"
+]
