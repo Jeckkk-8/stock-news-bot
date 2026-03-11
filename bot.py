c850008366eb8d44771a6ea41f1998e4ab53495e
Jeckkk-8 <dechason@hotmail.com>
2026-03-11 10:58:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,65 @@
-from news_engine import fetch_news
-from sentiment_engine import analyze
-from alpha_brief import generate
-from config import TOKEN,CHAT_ID
 import requests
-import feedparser
 import time
 import threading
 import os
 from flask import Flask
-from textblob import TextBlob
 
-TOKEN="8692112101:AAHa6X-3bCXpzRRsrbNGFNAM9-W6nLGNaHo"
-CHAT_ID="8386698996"
+from news_engine import fetch_news
+from sentiment_engine import analyze
+from alpha_brief import generate
+from config import TOKEN,CHAT_ID
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Investor Terminal running"
+    return "Investor Terminal Running"
 
-def send_telegram(msg):
-    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url,data={"chat_id":CHAT_ID,"text":msg})
+def send(msg):
+
+    url=f"https://api.telegram.org/bot{8692112101:AAHa6X-3bCXpzRRsrbNGFNAM9-W6nLGNaHo}/sendMessage"
+
+    requests.post(url,data={
+        "chat_id":CHAT_ID,
+        "text":msg
+    })
+
 
 def bot_loop():
 
     print("Investor Terminal started")
 
+    brief = generate()
+    send(brief)
+
     while True:
 
-news = fetch_news()
+        news = fetch_news()
 
-        time.sleep(300)  # เช็คข่าวทุก 5 นาที
-if __name__ == "__main__":
+        for item in news:
+
+            sentiment = analyze(item["title"])
+
+            msg=f"""
+⚡ BREAKING MARKET NEWS
+
+🏢 {item['ticker']}
+
+{item['title']}
+
+📊 Sentiment
+{sentiment}
+"""
+
+            send(msg)
+
+        time.sleep(300)
+
+
+if __name__=="__main__":
 
     threading.Thread(target=bot_loop).start()
 
-    port=int(os.environ.get("PORT",10000))
+    port=int(os.environ.get("PORT",8080))
 
     app.run(host="0.0.0.0",port=port)
