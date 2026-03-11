a1e1048d143b5e69c0aa25ca31c22214d15d39d7
Jeckkk-8 <dechason@hotmail.com>
2026-03-11 10:02:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,39 @@
+import requests
+import feedparser
 import time
+import threading
+import os
+from flask import Flask
+from textblob import TextBlob
 
-print("BOT STARTED")
+TOKEN="8692112101:AAHa6X-3bCXpzRRsrbNGFNAM9-W6nLGNaHo"
+CHAT_ID="PUT_CHAT_ID"
 
-while True:
-    print("BOT RUNNING...")
-    time.sleep(60)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Investor Terminal running"
+
+def send_telegram(msg):
+    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url,data={"chat_id":CHAT_ID,"text":msg})
+
+def bot_loop():
+
+    print("Investor Terminal started")
+
+    while True:
+
+        send_telegram("🚀 BOT ALIVE")
+
+        time.sleep(600)
+
+
+if __name__ == "__main__":
+
+    threading.Thread(target=bot_loop).start()
+
+    port=int(os.environ.get("PORT",10000))
+
+    app.run(host="0.0.0.0",port=port)
