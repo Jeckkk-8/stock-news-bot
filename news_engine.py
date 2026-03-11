d5c9187af950e6c8ffedfb80b86aeb10c3f6ef88
Jeckkk-8 <dechason@hotmail.com>
2026-03-11 12:16:15 +0700

    Update news_engine.py

--- news_engine.py
+++ news_engine.py
@@ -3,6 +3,24 @@ from newspaper import Article
 from deep_translator import GoogleTranslator
 from config import RSS_FEEDS,STOCKS
 
+COMPANY_NAMES={
+"NVDA":"nvidia",
+"AMZN":"amazon",
+"MSFT":"microsoft",
+"GOOGL":"google",
+"META":"meta",
+"TSM":"tsmc",
+"PLTR":"palantir",
+"CRWD":"crowdstrike",
+"LLY":"eli lilly",
+"RKLB":"rocket lab",
+"AVGO":"broadcom",
+"VRT":"vertiv",
+"CEG":"constellation",
+"OKLO":"oklo",
+"RBRK":"rubrik"
+}
+
 translator=GoogleTranslator(source="auto",target="th")
 
 def summarize(text):
